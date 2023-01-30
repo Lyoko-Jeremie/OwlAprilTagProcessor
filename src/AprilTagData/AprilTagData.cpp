e09d213efda7cd56b8d891c3f8415f9043410118
LyokoJeremie <lucheng989898@gmail.com>
2023-01-31 02:37:10 +0800

    AprilTagDataObject

--- src/AprilTagData/AprilTagData.cpp
+++ src/AprilTagData/AprilTagData.cpp
@@ -3,6 +3,7 @@
 #include "AprilTagData.h"
 #include <boost/core/ignore_unused.hpp>
 #include <boost/log/trivial.hpp>
+#include <utility>
 
 extern "C" {
 #include "apriltag.h"
@@ -27,7 +28,7 @@ namespace OwlAprilTagData {
             apriltag_detector_add_family(td, tf);
         }
 
-        void analysis(cv::Mat image) {
+        auto analysis(cv::Mat image) -> std::shared_ptr<AprilTagDataObject> {
             image_u8_t img_header = {.width = image.cols,
                     .height = image.rows,
                     .stride = image.cols,
@@ -42,18 +43,21 @@ namespace OwlAprilTagData {
 
                 // TODO Do stuff with detections here.
                 BOOST_LOG_TRIVIAL(trace)
-                        << "calcTag : " << i
-                        << " id " << det->id
-                        << " c[0]x " << det->c[0]
-                        << " c[1]y " << det->c[1]
-                        << " p[0] (" << det->p[0][0] << "," << det->p[0][0] << ")"
-                        << " p[1] (" << det->p[1][0] << "," << det->p[1][0] << ")"
-                        << " p[2] (" << det->p[2][0] << "," << det->p[2][0] << ")"
-                        << " p[3] (" << det->p[3][0] << "," << det->p[3][0] << ")"
-                        << " hamming " << det->hamming
-                        << " decision_margin " << det->decision_margin;
+                    << "calcTag : " << i
+                    << " id " << det->id
+                    << " c[0]x " << det->c[0]
+                    << " c[1]y " << det->c[1]
+                    << " p[0] (" << det->p[0][0] << "," << det->p[0][0] << ")"
+                    << " p[1] (" << det->p[1][0] << "," << det->p[1][0] << ")"
+                    << " p[2] (" << det->p[2][0] << "," << det->p[2][0] << ")"
+                    << " p[3] (" << det->p[3][0] << "," << det->p[3][0] << ")"
+                    << " hamming " << det->hamming
+                    << " decision_margin " << det->decision_margin;
 
             }
+
+            // TODO
+            return std::make_shared<AprilTagDataObject>();
         }
 
         ~AprilTagDataImpl() {
@@ -66,5 +70,15 @@ namespace OwlAprilTagData {
         apriltag_family_t *tf;
     };
 
+    auto AprilTagDataObject2Map(std::shared_ptr<AprilTagDataObject> o) -> std::map<std::string, std::string> && {
+        std::map<std::string, std::string> m;
+        // TODO
+        m.try_emplace("", "");
+        return std::move(m);
+    }
+
+    std::shared_ptr<AprilTagDataObject> AprilTagData::analysis(cv::Mat image) {
+        return impl->analysis(std::move(image));
+    }
 } // OwlAprilTagData
 
--- src/AprilTagData/AprilTagData.h
+++ src/AprilTagData/AprilTagData.h
@@ -10,9 +10,18 @@ namespace OwlAprilTagData {
 
     struct AprilTagDataImpl;
 
+    // TODO
+    struct AprilTagDataObject {
+        // TODO
+    };
+
+    auto AprilTagDataObject2Map(std::shared_ptr<AprilTagDataObject> o) -> std::map<std::string, std::string> &&;
+
     class AprilTagData : public std::enable_shared_from_this<AprilTagData> {
     public:
         AprilTagData() = default;
+
+        std::shared_ptr<AprilTagDataObject> analysis(cv::Mat image);
 
     private:
         std::shared_ptr<AprilTagDataImpl> impl{};
